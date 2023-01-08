d764f3207369f512d273347ee70ed213b0ae4931
mapned <110787440+mapned@users.noreply.github.com>
2023-01-08 20:23:40 +0200

    Update get-with-token.sh

--- get-with-token.sh
+++ get-with-token.sh
@@ -1,7 +1,7 @@
 set -e
-read -p "Enter repository url, without https and with .git (for ex. github.com/someone/repo.git): " repo
-read -p "Enter your username: " username
-read -p "Enter access token: " token
+read -ep "Enter repository url, without https and with .git (for ex. github.com/someone/repo.git): " repo
+read -ep "Enter your username: " username
+read -ep "Enter access token: " token
 echo "Cloning given git repo..."
 git clone https://${username}:${token}@${repo}
 echo "Done."
